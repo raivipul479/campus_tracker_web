9609ae58e8512c886242b0a76d937ada489dfd53
nitish347 <nitishchaurasiya3470@gmail.com>
2026-07-07 02:00:07 +0530

    Allow new Render preview host

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   preview: {
-    allowedHosts: ['location-app-tfks.onrender.com']
+    allowedHosts: [
+      'location-app-tfks.onrender.com',
+      'location-app-1-61r2.onrender.com'
+    ]
   }
 });
